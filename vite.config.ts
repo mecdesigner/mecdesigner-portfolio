3f1a90b83eb037a13416722ff8bf19411b34b071
mecdesigner <info@mecdesigner.com>
2025-10-31 12:21:44 +0100

    Updating new Home Page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GH_PAGES ? '/REPO_NAME/' : '/',
+  base: '/mecdesigner-portfolio/',
 })
