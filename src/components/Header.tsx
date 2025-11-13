eac0af4ea6e68d20f2e6e271d3577c4c7b11c737
mecdesigner <4757077+mecdesigner@users.noreply.github.com>
2025-11-13 14:51:20 +0100

    Reintegrate default Navigation Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,168 +1,145 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef } from 'react'
+import { useMegaPosition } from '../lib/useMegaPosition'
 import { Link, NavLink } from 'react-router-dom'
-import { IconMenu2, IconX, IconChevronDown } from '@tabler/icons-react'
+import { IconChevronDown, IconMenu2, IconX } from '@tabler/icons-react'
+import { asset } from '../lib/asset'
 import CTA from '../components/CTA'
 
 const base = import.meta.env.BASE_URL
 const href = (p: string) => `${base}${p.replace(/^\/+/, '')}`
 
 export default function Header() {
-  
+  // refs for the two mega menus
+  const caseStudiesRef = useRef<HTMLDivElement>(null)
+  const credentialsRef  = useRef<HTMLDivElement>(null)
+  useMegaPosition(caseStudiesRef)
+  useMegaPosition(credentialsRef)
   return (
-    <div className="nav-container">
-      <div>
-          <div className="bar bar--sm visible-xs original--bg">
-              <div className="container">
-                  <div className="row">
-                      <div className="col-9 col-md-2">
-                      <Link to="/" aria-label="Home">
-  <img className="logo logo-dark" alt="Mecdesigner logo dark" src="img/logo-mecdesigner-dark.png" />
-  <img className="logo logo-light" alt="Mecdesigner logo light" src="img/logo-mecdesigner-light.png" />
-</Link>
-                      </div>
-                      <div className="col-3 col-md-10 text-right">
-                      <a href="#" className="hamburger-toggle" data-toggle-class="#menu1;hidden-xs hidden-sm" aria-label="Toggle menu"> 
-                                    <i className="icon icon--sm stack-interface stack-menu" aria-hidden="true"></i> 
-                                </a>
-                      </div>
-                  </div>
+    <header className="nav-container">
+      <nav id="menu1" className="bar bar--sm bar-1 pos-fixed original--bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-2 col-md-2">
+              <div className="bar__module">
+                <Link to="/">
+                  <img className="logo logo-dark" alt="Mecdesigner logo dark" src="img/logo-mecdesigner-dark.png" />
+                  <img className="logo logo-light" alt="Mecdesigner logo light" src="img/logo-mecdesigner-light.png" />
+                </Link>
               </div>
-          </div>
-          <nav id="menu1" className="bar bar--sm bar-1 hidden-xs pos-fixed original--bg" data-scroll-class="200px:pos-fixed">
-              <div className="container">
-                  <div className="row">
-                      <div className="col-lg-2 col-md-2 hidden-xs">
-                          <div className="bar__module">
-                          <Link to="/" aria-label="Home">
-  <img className="logo logo-dark" alt="Mecdesigner logo dark" src="img/logo-mecdesigner-dark.png" />
-  <img className="logo logo-light" alt="Mecdesigner logo light" src="img/logo-mecdesigner-light.png" />
-</Link>
-                          </div>
-                      </div>
-                      <div className="col-lg-10 col-md-12 text-right text-left-xs text-left-sm">
-                          <div className="bar__module">
-                              <ul className="menu-horizontal text-left">
-                                  <li> 
-                                      <a href="about">
-                                      About
-                                      </a> 
-                                  </li>
-                                  <li> 
-                                      <a href="/design-system/">
-                                      Design system
-                                      </a> 
-                                  </li>
-                                  <li className="dropdown dropdown--hover"> 
-                                      <span className="dropdown__trigger">
-                                      Case studies
-                                      </span>
-                                      <div className="dropdown__container">
-                                          <div className="container">
-                                              <div className="row">
-                                                  <div className="dropdown__content row w-100">
-                                                      <div className="col-md-4 col-sm-12">
-                                                          <h5>UI &amp; Web Design</h5>
-                                                          <ul className="menu-vertical">
-                                                              <li> 
-                                                                  <a href="globaldesignsystem">&#129513; BBG Global Design System</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="altroconsumo">&#128236; Altroconsumo</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="asperbrothers">&#128187; Asper Brothers</a> 
-                                                              </li>
-                                                          </ul>
-                                                      </div>
-                                                      <div className="col-md-4 col-sm-12">
-                                                          <h5>UX &amp; Research</h5>
-                                                          <ul className="menu-vertical">
-                                                              <li> 
-                                                                  <a href="ux-analysis">&#128270; BBG UX Analysis</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="wheeshing">&#128734; Wheeshing</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="helpicam">&#128247; Helpicam</a> 
-                                                              </li>
-                                                          </ul>
-                                                      </div>
-                                                      <div className="col-md-4 col-sm-12">
-                                                          <h5>Other</h5>
-                                                          <ul className="menu-vertical">
-                                                              <li> 
-                                                                  <a href="outsourcing-guide">&#128161; Outsourcing Guide</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="mybank">&#127974; MyBank</a> 
-                                                              </li>
-                                                          </ul>
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </li>
-                                  <li className="dropdown dropdown--hover"> 
-                                      <span className="dropdown__trigger">
-                                      Credentials
-                                      </span>
-                                      <div className="dropdown__container">
-                                          <div className="container">
-                                              <div className="row">
-                                                  <div className="dropdown__content row w-100">
-                                                      <div className="col-md-6 col-sm-12">
-                                                          <h5>Articles</h5>
-                                                          <ul className="menu-vertical">
-                                                              <li> 
-                                                                  <a href="https://medium.com/@andreamecenero/a-story-about-ux-analysis-4dd019f557c9" target="_blank">&#128210; A story about UX Analysis</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="https://medium.com/@andreamecenero/a-story-about-ux-scenario-5133b008f989" target="_blank">&#128210; A story about UX scenario</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="https://medium.com/@andreamecenero/how-i-went-from-web-design-to-ui-ux-and-why-you-should-too-caebf257109a" target="_blank">&#128210; How I Went from Web Design to UI/UX</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="https://medium.com/@andreamecenero/how-a-graphic-designer-can-become-a-web-expert-c283e48381bf" target="_blank">&#128210; How I Transitioned from Graphic Design to Web Design</a> 
-                                                              </li>
-                                                              <li> 
-                                                                  <a href="https://asperbrothers.com/blog/design-system-in-figma/" target="_blank">&#128240; Design System in Figma</a> 
-                                                              </li>
-                                                          </ul>
-                                                      </div>
-                                                      <div className="col-md-6 col-sm-12">
-                                                          <h5>Certificates</h5>
-                                                          <ul className="menu-vertical">
-                                                              <li> 
-                                                                  <a href="https://www.credly.com/users/andrea-mecenero" target="_blank">&#128220; Credly Dashboard</a> 
-                                                              </li>
-                                                          </ul>
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </li>
-                                  
+            </div>
+            <div className="col-lg-10 col-md-12 text-right text-left-xs text-left-sm">
+              <div className="bar__module">
+
+                <ul className="menu-horizontal text-left">
+                  <li>
+                    <NavLink to="/about">About</NavLink>
+                  </li>
+
+                  <li>
+                    {/* If this is a static subfolder in /public/design-system, use href(); 
+                      if it's a React route, replace with <NavLink to="/design-system">…</NavLink> */}
+                    <a href={href('design-system/')}>Design system</a>
+                  </li>
+
+                  {/* CASE STUDIES (mega dropdown) */}
+                  <li className="dropdown dropdown--hover">
+                    <span className="dropdown__trigger" tabIndex={0}>
+                      Case studies
+                      <IconChevronDown size={16} stroke={1.75} aria-hidden="true" style={{ marginLeft: 6 }} />
+                    </span>
+
+                    <div className="dropdown__container" ref={caseStudiesRef}>
+                      <div className="container">
+                        <div className="row">
+                          <div className="dropdown__content row w-100">
+                            <div className="col-md-4 col-sm-12">
+                              <h5>UI &amp; Web Design</h5>
+                              <ul className="menu-vertical">
+                                <li><NavLink to="/globaldesignsystem">&#129513; BBG Global Design System</NavLink></li>
+                                <li><NavLink to="/altroconsumo">&#128236; Altroconsumo</NavLink></li>
+                                <li><NavLink to="/asperbrothers">&#128187; Asper Brothers</NavLink></li>
                               </ul>
+                            </div>
+
+                            <div className="col-md-4 col-sm-12">
+                              <h5>UX &amp; Research</h5>
+                              <ul className="menu-vertical">
+                                <li><NavLink to="/ux-analysis">&#128270; BBG UX Analysis</NavLink></li>
+                                <li><NavLink to="/wheeshing">&#128734; Wheeshing</NavLink></li>
+                                <li><NavLink to="/helpicam">&#128247; Helpicam</NavLink></li>
+                              </ul>
+                            </div>
+
+                            <div className="col-md-4 col-sm-12">
+                              <h5>Other</h5>
+                              <ul className="menu-vertical">
+                                <li><NavLink to="/outsourcing-guide">&#128161; Outsourcing Guide</NavLink></li>
+                                <li><NavLink to="/mybank">&#127974; MyBank</NavLink></li>
+                              </ul>
+                            </div>
                           </div>
-                          <div className="bar__module">
-                              <a className="btn btn--sm btn--primary type--uppercase" href="mailto:info@mecdesigner.com?subject=Hello I want a free consultation"> 
-                                  <span className="btn__text">
-                                  Contact me
-                                  </span>
-                              </a>
-                          </div>
+                        </div>
                       </div>
-                  </div>
+                    </div>
+                  </li>
+
+                  {/* CREDENTIALS (dropdown with external links) */}
+                  <li className="dropdown dropdown--hover">
+                    <span className="dropdown__trigger" tabIndex={0}>
+                      Credentials
+                      <IconChevronDown size={16} stroke={1.75} aria-hidden="true" style={{ marginLeft: 6 }} />
+                    </span>
+
+                    <div className="dropdown__container" ref={credentialsRef}>
+                      <div className="container">
+                        <div className="row">
+                          <div className="dropdown__content row w-100">
+                            <div className="col-md-6 col-sm-12">
+                              <h5>Articles</h5>
+                              <ul className="menu-vertical">
+                                <li><a href="https://medium.com/@andreamecenero/a-story-about-ux-analysis-4dd019f557c9" target="_blank" rel="noreferrer">&#128210; A story about UX Analysis</a></li>
+                                <li><a href="https://medium.com/@andreamecenero/a-story-about-ux-scenario-5133b008f989" target="_blank" rel="noreferrer">&#128210; A story about UX scenario</a></li>
+                                <li><a href="https://medium.com/@andreamecenero/how-i-went-from-web-design-to-ui-ux-and-why-you-should-too-caebf257109a" target="_blank" rel="noreferrer">&#128210; How I Went from Web Design to UI/UX</a></li>
+                                <li><a href="https://medium.com/@andreamecenero/how-a-graphic-designer-can-become-a-web-expert-c283e48381bf" target="_blank" rel="noreferrer">&#128210; How I Transitioned from Graphic Design to Web Design</a></li>
+                                <li><a href="https://asperbrothers.com/blog/design-system-in-figma/" target="_blank" rel="noreferrer">&#128240; Design System in Figma</a></li>
+                              </ul>
+                            </div>
+
+                            <div className="col-md-3 col-sm-12">
+                              <h5>Certificates</h5>
+                              <ul className="menu-vertical">
+                                <li><a href="https://www.credly.com/users/andrea-mecenero" target="_blank" rel="noreferrer">&#128220; Credly Dashboard</a></li>
+                              </ul>
+                            </div>
+
+                            <div className="col-md-3 col-sm-12">
+                              <h5>Social</h5>
+                              <ul className="menu-vertical">
+                                <li><a href="https://www.linkedin.com/in/mecdesigner/" target="_blank" rel="noreferrer">&#128220; LinkedIn</a></li>
+                                <li><a href="https://www.upwork.com/freelancers/~011d35bf594a0aaef9?mp_source=share" target="_blank" rel="noreferrer">&#128220; UpWork</a></li>
+                                <li><a href="https://www.fiverr.com/s/rE8jdXb" target="_blank" rel="noreferrer">&#128220; Fiverr</a></li>
+                                <li><a href="#" target="_blank" rel="noreferrer">&#128220; Toptal</a></li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+
               </div>
-          </nav>
-      </div>
-  </div>
-
-
-
+              <CTA
+                href="https://calendly.com/andreamecenero/intro-call"
+                style="primary-1"
+                size="lg"
+                upper>
+                Free consultation
+              </CTA>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
--- src/lib/useMegaPosition.ts
+++ src/lib/useMegaPosition.ts
@@ -1,4 +1,3 @@
-// src/lib/useMegaPosition.ts
 import { RefObject, useLayoutEffect } from 'react'
 
 export function useMegaPosition(ref: RefObject<HTMLElement>) {
@@ -6,69 +5,36 @@ export function useMegaPosition(ref: RefObject<HTMLElement>) {
     const el = ref.current
     if (!el) return
 
-    // Match your grid breakpoint (~992px). Keep in sync with CSS (62em).
-    const mq = window.matchMedia('(min-width: 991px)')
-    const reset = () => { el.removeAttribute('style') }
-
     const update = () => {
-      // If mobile / menu collapsed → let CSS handle stacking
-      if (!mq.matches) {
-        reset()
-        return
-      }
-
-      // Anchor: <li> that wraps the dropdown container
+      // anchor = the <li> (parent of the dropdown container)
       const anchor = el.parentElement as HTMLElement | null
-      // Full-width grid container inside the nav bar
-      const bar = el.closest('.bar') as HTMLElement | null
-      const grid = bar?.querySelector('.container') as HTMLElement | null
-      if (!anchor || !grid) { 
-        reset(); 
-        return 
-    }
-
-      // If the menu module is collapsed (mobile), bail out
-      const module = el.closest('.nav-menu-module') as HTMLElement | null
-      if (module && module.classList.contains('toggled-class') && window.matchMedia('(max-width: 990px)').matches) {
-        reset()
-        return
-      }
+      // navBox = the full-width grid container inside the bar
+      const navBox = el.closest('.bar')?.querySelector('.container') as HTMLElement | null
+      if (!anchor || !navBox) return
 
       const a = anchor.getBoundingClientRect()
-      const g = grid.getBoundingClientRect()
+      const n = navBox.getBoundingClientRect()
 
-      // Align dropdown left edge to grid left, regardless of which column the menu sits in
-      const left = g.left - a.left
+      // Move the dropdown so its left edge matches the grid left,
+      // even when the menu sits in the right column.
+      const left = n.left - a.left
 
       el.style.position = 'absolute'
       el.style.top = `${anchor.offsetHeight}px`
       el.style.left = `${left}px`
       el.style.right = 'auto'
-      el.style.width = `${g.width}px`
+      el.style.width = `${n.width}px`
       el.style.transform = 'none'
       el.style.zIndex = '1000'
     }
 
-    // Initial + observers
     update()
-
     const ro = new ResizeObserver(update)
     ro.observe(document.body)
-    // also watch the bar/container widths more directly
-    const bar = el.closest('.bar') as HTMLElement | null
-    if (bar) ro.observe(bar)
-    const container = bar?.querySelector('.container') as HTMLElement | null
-    if (container) ro.observe(container)
-
-    // react to breakpoint changes & scroll
-    const onMQ = () => update()
-    mq.addEventListener?.('change', onMQ)
     window.addEventListener('resize', update, { passive: true })
     window.addEventListener('scroll', update, { passive: true })
-
     return () => {
       ro.disconnect()
-      mq.removeEventListener?.('change', onMQ)
       window.removeEventListener('resize', update)
       window.removeEventListener('scroll', update)
     }
